82f85ec1ce2bda5797e986cd42c26fd1817999ad
Pedro Martins <ace@Pedros-MacBook-Pro.local>
2019-02-20 13:52:33 -0300

    add redirect on browser language detection

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ WORKDIR $WORKDIR
 RUN mkdir -p $WORKDIR
 
 COPY ./src ./public
+COPY ./nginx.conf ./nginx.conf
+COPY ./http_lang.pm ./http_lang.pm
 
 FROM nginx:alpine-perl
 
@@ -20,7 +22,6 @@ RUN apk add --no-cache nano spawn-fcgi fcgiwrap wget curl
 # COPY ./dockerconfig/htaccess /etc/nginx/.htaccess
 COPY --from=builder /usr/src/app/nginx.conf /etc/nginx/nginx.conf
 COPY --from=builder /usr/src/app/public /var/www/html/
-
-RUN ls  /var/www/html/
+COPY --from=builder /usr/src/app/http_lang.pm /usr/lib/perl5/core_perl/http_lang.pm
 
 ENV PORT 8080
--- http_lang.pm
+++ http_lang.pm
@@ -1,6 +1,7 @@
 package http_lang;
 
 use nginx;
+use I18N::LangTags qw(:ALL);
 
 sub quality {
     my $lang = shift;
@@ -26,25 +27,39 @@ sub lang_only {
 sub choose {
     my @requested = split(/,\s*/, shift);
     my @supported = split(/,\s*/, shift);
+		my @supportedLocales = split(/,\s*/, shift);
 
-    my @order = sort { quality($b) <=> quality($a) } @requested;
+		my %locales;
+		$locales{$_}++ for (@supportedLocales);
+
+		my %supportedLang;
+		$supportedLang{$_}++ for (@supported);
+		
+    my @order = sort { quality($b) <=> quality($a) } @requested;		
     foreach my $lang (@order) {
-        $lang = lang_only($lang);
-        return $lang if ($lang ~~ @supported);
+				$locale = lc $lang;
+				$choose{locale} = lc $lang;
+				$choose{lang} = lang_only($lang);
+				if((!$locales{$locale})){
+					$choose{locale} = $choose{lang};
+					$choose{locale} = 'en-us' if(!$locales{$choose{lang}});
+				} 
+        return $choose if ($supportedLang{$choose{'lang'}});
     }
 
-    return $supported[0];
+		return $supported[0];
 }
 
 sub handler {
     my $r = shift;
 
-    my $supported = $r->variable('SUPPORTED_LANG') or "en";
+		my $supported = $r->variable('SUPPORTED_LANG') or "en";
+		my $supportedLocales = $r->variable('SUPPORTED_LOCALES') or "en-us";
     my $requested = $r->header_in('Accept-Language') or "en-US";
 
-    my $lang = choose($requested, $supported);
+		my $choose = choose($requested, $supported, $supportedLocales);
 
-    $r->internal_redirect('/' . $lang . $r->uri);
+		$r->internal_redirect('/redirect-' . $choose{locale} . $r->uri);
     return OK;
 }
 
@@ -52,16 +67,18 @@ sub debug {
     my $r = shift;
 
     my $supported = $r->variable('SUPPORTED_LANG') or "en";
+		my $supportedLocales = $r->variable('SUPPORTED_LOCALES') or "en";
     my $requested = $r->header_in('Accept-Language') or "en-US";
 
-    my $lang = choose($requested, $supported);
-
-    $r->send_http_header("text/plain");
+		$r->send_http_header("text/plain");
     return OK if $r->header_only;
 
-    $r->print('$supported = ' . $supported . "\n");
-    $r->print('$requested = ' . $requested . "\n");
-    $r->print('Redirect to: ' . '/' . $lang . $r->uri . "\n");
+		my $choose = choose($requested, $supported, $supportedLocales);
+
+		$r->print('$requested = ' . $requested . "\n");
+		$r->print('Redirect lang: ' . '/' .$choose{lang}  . "\n");
+    $r->print('Redirect to: ' . '/'. $choose{locale} . $r->uri . "\n");
+
     return OK;
 }
 
